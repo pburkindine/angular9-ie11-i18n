03820b647893f46402bf8d620b2b9a8bbff88a6a
Pete Burkindine <pete.burkindine@semanticbits.com>
2020-03-19 16:48:48 -0500

    initial commit

---
+++ apps/ie11-i18n-e2e/src/integration/app.spec.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('ie11-i18n', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome to ie11-i18n!');
+  });
+});
